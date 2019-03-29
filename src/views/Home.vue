411d4c31689468ff87be3333b72d9683d00f906a
Quéau Jean Pierre <queaujeanpierre@MacBook-Pro-de-Queau-Jean-Pierre.local>
2019-03-29 14:26:53 +0100

    update to @ionic/core4.1.2 & @ionic/vue0.0.4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,10 +11,18 @@
     <ion-content padding>
       <p>Test Flipimages Web component in Vue.js</p>
       <ion-button @click="goToAbout" size="large">Go to About</ion-button>
-      <ion-button @click="goToFlipImages('l_horizontal')" size="large">Flip L Images Horizontal</ion-button>
-      <ion-button @click="goToFlipImages('l_vertical')" size="large">Flip L Images Vertical</ion-button>
-      <ion-button @click="goToFlipImages('p_horizontal')" size="large">Flip P Images Horizontal</ion-button>
-      <ion-button @click="goToFlipImages('p_vertical')" size="large">Flip P Images Vertical</ion-button>
+      <ion-button @click="goToFlipImages('l_horizontal')" size="large"
+        >Flip L Images Horizontal</ion-button
+      >
+      <ion-button @click="goToFlipImages('l_vertical')" size="large"
+        >Flip L Images Vertical</ion-button
+      >
+      <ion-button @click="goToFlipImages('p_horizontal')" size="large"
+        >Flip P Images Horizontal</ion-button
+      >
+      <ion-button @click="goToFlipImages('p_vertical')" size="large"
+        >Flip P Images Vertical</ion-button
+      >
     </ion-content>
   </ion-app>
 </template>
