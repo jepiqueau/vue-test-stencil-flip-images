ee2db854e162085fac75ce3a48f6e53b27490531
Quéau Jean Pierre <queaujeanpierre@MacBook-Pro-de-Queau-Jean-Pierre.local>
2019-04-04 10:42:50 +0200

    implement IonicVueRouter

--- src/main.js
+++ src/main.js
@@ -5,18 +5,16 @@ import store from "./store";
 import { defineCustomElements as flipimages } from "stencil-flipimages/dist/loader";
 
 import Ionic from "@ionic/vue";
+import "@ionic/core/css/ionic.bundle.css";
 
 Vue.config.ignoredElements = [/jeep-\w*/];
-Vue.config.ignoredElements = [/ion-\w*/];
 Vue.use(Ionic);
 flipimages(window);
 
 Vue.config.productionTip = false;
-
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount("#app");
-
--- src/views/FlipImages.vue
+++ src/views/FlipImages.vue
@@ -1,13 +1,9 @@
 <template>
-  <ion-app>
+  <div class="ion-page">
     <ion-header>
       <ion-toolbar color="primary">
         <ion-buttons slot="start">
-          <!--
-                *** does not work
-            <ion-back-button defaultHref="/"></ion-back-button>
-            -->
-          <ion-button @click="goBackHome()" size="small">Back</ion-button>
+          <ion-back-button defaultHref="/"></ion-back-button>
         </ion-buttons>
         <ion-title>Flipimages</ion-title>
       </ion-toolbar>
@@ -53,7 +49,7 @@
         </jeep-flipimages>
       </template>
     </ion-content>
-  </ion-app>
+  </div>
 </template>
 
 <script>
@@ -63,11 +59,6 @@ export default {
     return {
       type: this.$route.params.type
     };
-  },
-  methods: {
-    goBackHome: function() {
-      this.$router.push("/");
-    }
   }
 };
 </script>
